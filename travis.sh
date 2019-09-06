fbc755b675edfff50889942ab824d5238cee5661
clienthax <clienthax@gmail.com>
2019-09-06 17:56:53 +0100

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ export ARCH=arm64;
 export SUBARCH=arm64;
 STRIP="aarch64-linux-android-4.9/bin/aarch64-linux-android-strip";
 export CCOMPILE=$CROSS_COMPILE;
-export CROSS_COMPILE=aarch64-linux-gnu-;
+export CROSS_COMPILE=aarch64-linux-android-;
 export PATH=$PATH:./aarch64-linux-android-4.9/bin/;
 make stealth_defconfig;
 make -j$(nproc --all);
